a89f0a1be564e9920b34d8b486992c88e11678a7
p.devv <pd27052003@gmail.com>
2024-11-09 21:54:33 +0530

    design theme and global css

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl">StoreIt-The only solution you need</h1>
+    <div className="flex-center h-screen">
+    <h1 className="body-2 ">StoreIt-The only solution you need</h1>
+    </div>
   );
 }
